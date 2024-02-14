f19eac75dcaad335d469465a43608c8325c4dd57
Muhammad.Zikria <Mzikria@myseneca.ca>
2024-02-13 22:39:11 -0500

    update lab3y

---
+++ lab3.py
@@ -0,0 +1,14 @@
+def write_file_with_name(file_path, name):
+    with open(file_path, 'w') as file:
+        file.write(f"Repository and git hub test file / n Good bye.")
+
+if __name__ == "__main__":
+    # Provide the file path and your name
+    file_path = "mzikria.txt"  # Change this to the desired file path
+    your_name = "zaki"   # Change this to your actual name
+
+    # Call the function to write the file
+    write_file_with_name(file_path, your_name)
+
+    print(f"File '{file_path}' this file has been created.")
+print('Ops445_lab2b by "MZikria"')
